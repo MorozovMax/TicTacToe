86a946e719cd62e870d5358b9bb4e103289f09be
Kirill Pavlishin <kirill1999super15@gmail.com>
2023-05-01 09:58:19 +0300

    Update dodo.py file

--- dodo.py
+++ dodo.py
@@ -1,19 +1,17 @@
-"""
-Module for automation of the assembly process
-"""
+"""Module for automation of the assembly process."""
 
 import glob
 from typing import Dict, Any
 import tomli
+from doit.tools import create_folder
 
 
-DOIT_CONFIG = {'default_tasks': ['all'],
-               'exclude': ['gitclean', 'wheel']}
+DOIT_CONFIG = {'default_tasks': ['all']}
 
 
 def dumpkeys(infile: str, table: str, outfile: str) -> None:
     """
-    Dumps TOML table keys one per line.
+    Dump TOML table keys one per line.
 
     :param infile: input file name
     :type infile: class: `str`
@@ -24,7 +22,7 @@ def dumpkeys(infile: str, table: str, outfile: str) -> None:
     """
     with open(infile, "rb") as fin:
         full = tomli.load(fin)
-    with open(outfile, "w") as fout:
+    with open(outfile, "w", encoding='utf-8') as fout:
         print(*full[table], sep="\n", file=fout)
 
 
@@ -48,7 +46,8 @@ def task_html() -> Dict[str, Any]:
     :rtype: class `Dict[str, Any]`
     """
     return {
-        'actions': ['sphinx-build -M html doc build'],
+        'actions': [(create_folder, ['server_dir/database']),
+                    'sphinx-build -M html doc build']
     }
 
 
@@ -60,8 +59,8 @@ def task_pot() -> Dict[str, Any]:
     :rtype: class `Dict[str, Any]`
     """
     return {
-        'actions': ['pybabel extract -o tictactoe.pot app/*.py'],
-        'file_dep': glob.glob('app/*.py'),
+        'actions': ['pybabel extract -o tictactoe.pot OnlineTicTacToe/*.py'],
+        'file_dep': glob.glob('OnlineTicTacToe/*.py'),
         'targets': ['tictactoe.pot'],
     }
 
@@ -88,9 +87,42 @@ def task_mo() -> Dict[str, Any]:
     :rtype: class `Dict[str, Any]`
     """
     return {
-        'actions': ['pybabel compile -D tictactoe -l ru -i locale/ru/LC_MESSAGES/tictactoe.po -d locale'],
-        'file_dep': ['locale/ru/LC_MESSAGES/tictactoe.po'],
-        'targets': ['locale/ru/LC_MESSAGES/tictactoe.mo'],
+      'actions': [(create_folder, ['OnlineTicTacToe/locale/ru/LC_MESSAGES']),
+                  'pybabel compile -D tictactoe -l ru -i locale/ru/LC_MESSAGES/tictactoe.po -d OnlineTicTacToe/locale'],
+      'file_dep': ['locale/ru/LC_MESSAGES/tictactoe.po'],
+      'targets': ['OnlineTicTacToe/locale/ru/LC_MESSAGES/tictactoe.mo'],
+    }
+
+
+def task_create_setup() -> Dict[str, Any]:
+    """
+    Create Setup file.
+
+    :return: managing dictionary
+    :rtype: class `Dict[str, Any]`
+    """
+    return {
+        'actions': ['cp setup/OnlineTicTacToe.spec TicTacToe',
+                    'pyinstaller --distpath setup/dist --workpath setup/build setup/setup.spec',
+                    'mv setup/dist/Setup TicTacToe/Setup'],
+        'file_dep': ['setup/OnlineTicTacToe.spec', 'setup/setup.py', 'setup/setup.spec'],
+        'task_dep': ['wheel'],
+        'targets': ['TicTacToe/Setup', 'TicTacToe/OnlineTicTacToe.spec'],
+    }
+
+
+def task_create_arch() -> Dict[str, Any]:
+    """
+    Create archive with setup distributive.
+
+    :return: managing dictionary
+    :rtype: class `Dict[str, Any]`
+    """
+    return {
+        'actions': ['tar -czvf OnlineTicTacToe.tar.gz TicTacToe',
+                    'mv TicTacToe/*.whl .'],
+        'task_dep': ['create_setup'],
+        'targets': ['OnlineTicTacToe.tar.gz'],
     }
 
 
@@ -116,7 +148,9 @@ def task_wheel() -> Dict[str, Any]:
     :rtype: class `Dict[str, Any]`
     """
     return {
-        'actions': ['python -m build -n -w'],
+        'actions': [(create_folder, ['TicTacToe']),
+                    'python -m build -n -w',
+                    'mv dist/*.whl TicTacToe'],
         'task_dep': ['mo', 'requirements'],
     }
 
@@ -129,10 +163,11 @@ def task_style_flake() -> Dict[str, Any]:
     :rtype: class `Dict[str, Any]`
     """
     return {
-        'actions': ['flake8 app/authentification_page.py', 'flake8 app/game_page.py',
-                    'flake8 app/search_game_page.py', 'flake8 app/setting_page.py',
-                    'flake8 app/start_page.py', 'flake8 app/tictactoe.py', 'flake8 server.py',
-                    'flake8 dodo.py']
+        'actions': ['flake8 OnlineTicTacToe/authentification_page.py', 'flake8 OnlineTicTacToe/game_page.py',
+                    'flake8 OnlineTicTacToe/search_game_page.py', 'flake8 OnlineTicTacToe/setting_page.py',
+                    'flake8 OnlineTicTacToe/start_page.py', 'flake8 OnlineTicTacToe/tictactoe.py',
+                    'flake8 server_dir/server.py', 'flake8 dodo.py', 'flake8 setup/setup.py',
+                    'flake8 OnlineTicTacToe/__main__.py']
     }
 
 
@@ -144,10 +179,11 @@ def task_style_pylint() -> Dict[str, Any]:
     :rtype: class `Dict[str, Any]`
     """
     return {
-        'actions': ['pylint app/authentification_page.py', 'pylint app/game_page.py',
-                    'pylint app/search_game_page.py', 'pylint app/setting_page.py',
-                    'pylint app/start_page.py', 'pylint app/tictactoe.py', 'pylint server.py',
-                    'pylint dodo.py']
+        'actions': ['pylint OnlineTicTacToe/authentification_page.py', 'pylint OnlineTicTacToe/game_page.py',
+                    'pylint OnlineTicTacToe/search_game_page.py', 'pylint OnlineTicTacToe/setting_page.py',
+                    'pylint OnlineTicTacToe/start_page.py', 'pylint OnlineTicTacToe/tictactoe.py',
+                    'pylint server_dir/server.py', 'pylint dodo.py', 'pylint setup/setup.py',
+                    'pylint OnlineTicTacToe/__main__.py']
     }
 
 
@@ -159,10 +195,11 @@ def task_docstyle() -> Dict[str, Any]:
     :rtype: class `Dict[str, Any]`
     """
     return {
-        'actions': ['pydocstyle app/authentification_page.py', 'pydocstyle app/game_page.py',
-                    'pydocstyle app/search_game_page.py', 'pydocstyle app/setting_page.py',
-                    'pydocstyle app/start_page.py', 'pydocstyle app/tictactoe.py', 'pydocstyle server.py',
-                    'pydocstyle dodo.py']
+        'actions': ['pydocstyle OnlineTicTacToe/authentification_page.py', 'pydocstyle OnlineTicTacToe/game_page.py',
+                    'pydocstyle OnlineTicTacToe/search_game_page.py', 'pydocstyle OnlineTicTacToe/setting_page.py',
+                    'pydocstyle OnlineTicTacToe/start_page.py', 'pydocstyle OnlineTicTacToe/tictactoe.py',
+                    'pydocstyle server_dir/server.py', 'pydocstyle dodo.py', 'pydocstyle setup/setup.py',
+                    'pydocstyle OnlineTicTacToe/__main__.py']
     }
 
 
@@ -175,7 +212,7 @@ def task_check() -> Dict[str, Any]:
     """
     return {
         'actions': None,
-        'task_dep': ['style_flake', 'docstyle', 'style_pylint']
+        'task_dep': ['style_flake', 'style_pylint', 'docstyle']
     }
 
 
@@ -188,5 +225,36 @@ def task_all() -> Dict[str, Any]:
     """
     return {
         'actions': None,
-        'task_dep': ['check', 'html', 'wheel']
+        'task_dep': ['html', 'check', 'create_arch']
+    }
+
+
+def task_run_server() -> Dict[str, Any]:
+    """
+    Run server.
+
+    :return: managing dictionary
+    :rtype: class `Dict[str, Any]`
+    """
+    return {
+        'actions': ['sudo docker compose -f server_dir/docker-compose.yml up -d'],
+        'file_dep': ['server_dir/server.py',
+                     'server_dir/run_server.sh',
+                     'server_dir/Pipfile',
+                     'server_dir/Dockerfile',
+                     'server_dir/docker-compose.yml'],
+        'uptodate': [False]
+    }
+
+
+def task_stop_server() -> Dict[str, Any]:
+    """
+    Stop server.
+
+    :return: managing dictionary
+    :rtype: class `Dict[str, Any]`
+    """
+    return {
+        'actions': ['docker stop tictactoe_server'],
+        'uptodate': [False]
     }
